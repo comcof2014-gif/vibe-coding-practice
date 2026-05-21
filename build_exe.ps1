eb40c2cf562a89383974ee14c9431536b7ffa026
comcof2014-gif <comcof2014@gmail.com>
2026-05-22 00:52:49 +0900

    Use baseline CPU flags for local EXE builds

--- build_exe.ps1
+++ build_exe.ps1
@@ -1,7 +1,10 @@
 $ErrorActionPreference = "Stop"
 
-py -m pip install --upgrade pip
-py -m pip install -r requirements.txt pyinstaller
+$env:CMAKE_ARGS = "-DGGML_NATIVE=OFF -DLLAMA_NATIVE=OFF -DGGML_AVX=OFF -DGGML_AVX2=OFF -DGGML_FMA=OFF -DGGML_F16C=OFF"
+$env:FORCE_CMAKE = "1"
+
+py -m pip install --upgrade pip setuptools wheel cmake ninja
+py -m pip install --no-binary llama-cpp-python --upgrade --force-reinstall --no-cache-dir -r requirements.txt pyinstaller
 
 py -m PyInstaller `
   --clean `
